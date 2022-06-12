99a2bc64d2b15dddaa1879b1f88b3fa3544bc0c8
kenny <kenny.luque.t@uni.pe>
2022-06-12 16:07:08 -0500

    doc api /

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ const categoriesRouter = require('./categories.router');
 const routerApi = app => {
     const router = express.Router();
 
+    app.use('/', (req, res ) => {
+        res.send('Hello World');
+    })
+
     app.use('/api/v1', router);
 
     router.use('/products', productsRouter);
